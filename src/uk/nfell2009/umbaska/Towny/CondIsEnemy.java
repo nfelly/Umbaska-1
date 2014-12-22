c9212b47101d74a930d45e43b17c40a4eeda92e8
portapipe <portapipe@gmail.com>
2014-12-22 11:38:40 +0100

    Fixed CondIsEnemy

--- src/uk/nfell2009/umbaska/Towny/CondIsEnemy.java
+++ src/uk/nfell2009/umbaska/Towny/CondIsEnemy.java
@@ -22,10 +22,11 @@ import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
 
 
-public class CondIsNeutral extends Condition {
+public class CondIsEnemy extends Condition {
 	 
 	 
     private Expression<String> twn1;
+    private Expression<String> twn2;
 
     @SuppressWarnings("unchecked")
     @Override
@@ -41,24 +42,41 @@ public class CondIsNeutral extends Condition {
 
     @Override
     public boolean check(Event e) {
-    		String tw1 = twn1.getSingle(e);
-    		Town t1 = null;
-            try {
-				t1 = TownyUniverse.getDataSource().getTown(tw1);
-			} catch (NotRegisteredException e1) {
-				e1.printStackTrace();
-			}
-    		Nation n1 = null;
-			try {
-				n1 = t1.getNation();
-			} catch (NotRegisteredException e1) {
-				e1.printStackTrace();
-			}
-            Boolean out = false;
-            if (n1.isEnemy()) {
-            	out = true;
-            }
-            return (out);
+
+		String tw1 = twn1.getSingle(e);
+		String tw2 = twn2.getSingle(e);
+		Town t1 = null;
+		Town t2 = null;
+        try {
+			t1 = TownyUniverse.getDataSource().getTown(tw1);
+		} catch (NotRegisteredException e1) {
+			e1.printStackTrace();
+		}
+        try {
+			t2 = TownyUniverse.getDataSource().getTown(tw2);
+		} catch (NotRegisteredException e1) {
+			e1.printStackTrace();
+		}
+		Nation n1 = null;
+		try {
+			n1 = t1.getNation();
+		} catch (NotRegisteredException e1) {
+			e1.printStackTrace();
+		}
+		Nation n2 = null;
+		try {
+			n2 = t2.getNation();
+		} catch (NotRegisteredException e1) {
+			e1.printStackTrace();
+		}
+        Boolean out = false;
+        if (n1.hasEnemy(n2)) {
+        	out = true;
+        	return (out);
+        } else {
+        	out = false;
+        	return (out);
+        }
             
             
     }
